de71b92de0fa0881969d8af64bec51fc842b489f
FelipeSousa-doc <felipesousaalvesfs@gmail.com>
2026-06-23 17:56:26 -0300

    commit inicial

---
+++ src/operations/Operations.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operations;
+
+/**
+ *
+ * @author slabj
+ */
+public class Operations {
+    public double firstNumber;
+    public double secondNumber;
+    public double sumNumbers(){
+        return firstNumber+secondNumber;
+    }
+    
+    public double minusNumbers(){
+        return firstNumber-secondNumber;
+    }
+    
+    public double timesNumbers(){
+        return firstNumber*secondNumber;
+    }
+    
+    public double divisionNumbers(){
+        return firstNumber/secondNumber;
+    }
+}
